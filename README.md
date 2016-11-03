297f9ef4718849febf47ffb5f31d6cee111c0d9f
Pau Giner <pau.giner@gmail.com>
2016-11-03 19:46:11 +0100

    Initial version
    Skeleton for a design guide

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # design-guide
-Design guide skeleton
+Work in progress document and collection of design resources for Wikimedia projects.
+
+Check it live at https://github.com/pauginer/design-guide/
---
+++ script/app.js
@@ -0,0 +1,48 @@
+function ensureMainElement(){ //Moves content inside a <main> element if they are not already
+  var hasMain = $("body > main").length == 1;
+  if (!hasMain){
+    var mainElt = $("<main>");
+    $("body").contents().appendTo(mainElt);
+    $("body").append(mainElt);
+  }
+}
+
+function loadMenu(){ //loads the contents of menu.html
+  ensureMainElement();
+  var link = $('link[rel="import"][href^="menu"]');
+  if (link.length > 0){
+    var content = link[0].import;
+    var el = $(content).find('main').contents();
+    var target = $("<nav class='menu'>");
+    target.append(el.clone());
+    target.prependTo($("body"));
+  } else{
+    console.error("Menu could not be loaded.");
+  }
+}
+
+function markCurrentSection(){
+  var pathName = window.location.pathname.substring(1);
+  var fileName = !!pathName.length?pathName:"index.html";
+  var current = $(".toc a[href$='"+fileName+"']").parent("li");
+  current.addClass("active");
+  //Expand subsections
+  var subSections = $("<ul class='subsections'>");
+  $("section > h2").each(function(i,e){
+    var subItem = $("<a>");
+    subItem.html($(e).html());
+    subItem.attr("href","#section"+i);
+    $(e).attr("id","section"+i)
+    $("<li>").append(subItem).appendTo(subSections);
+  });
+  //subSections.appendTo(current);
+  subSections.insertAfter(current);
+}
+
+function main(){
+  loadMenu();
+  markCurrentSection();
+}
+
+
+$(main);
